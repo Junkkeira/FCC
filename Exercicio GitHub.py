1445311bd1aba9d0af263c2322b643452935aeac
Junkkeira <94480584+Junkkeira@users.noreply.github.com>
2021-11-16 20:13:06 -0300

    Add files via upload

---
+++ Exercicio GitHub.py
@@ -0,0 +1,25 @@
+'''
+Elabore um programa que calcule e apresente a soma dos inteiros existentes entre dois valores
+lidos. Considere que o segundo número lido deve ser maior que o primeiro número lido.
+'''
+#Variáveis
+som=0
+x=0
+
+#Input
+a=int(input('insira um valor para A:'))
+b=int(input('Insira um valor maior que A para B:'))
+
+#Resolução das Respostas de acordo com as Condições
+#Output
+if b>a:
+    for i in range(a+1,b,1):
+        som=som+i
+    print(som)
+else:
+    print('O segundo valor deve ser obrigatoriamente maior que o primeiro')
+    a = int(input('insira um valor para A:'))
+    b = int(input('Insira um valor maior que A para B:'))
+    for i in range(a+1,b,1):
+        som=som+i
+    print(som)
